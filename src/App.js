a31a60bc4cc55666f3750d9849969adb24750f3c
Elias K <127874391+n2koel00@users.noreply.github.com>
2023-10-13 21:16:04 +0300

    Valuttalaskuri

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import axios from 'axios';
+
+//defaultkey: fca_live_8FdPyp64BEEdgEPM5iclsz2N0MCVCqKxTs9SQ9ot
+
+const URL = "https://api.freecurrencyapi.com/v1/latest?apikey=fca_live_8FdPyp64BEEdgEPM5iclsz2N0MCVCqKxTs9SQ9ot&currencies=GBP%2CEUR&base_currency=EUR"
+
 
 function App() {
+  const [eur, setEur] = useState(0);
+  const [gbp, setGbp] = useState(0);
+  const [rate, setRate] = useState(0);
+  const [rateE,setRateE] = useState(0);
+  
+  const convert = (e) => {
+    e.preventDefault()
+    axios.get(URL)
+      .then((response) => {
+        const json = response.data
+        setRate(json.data.GBP);
+        setGbp(eur * json.data.GBP);
+        setRateE(json.data.EUR)
+      }).catch (error => {
+        alert(error)
+      })
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="container">
+      <form onSubmit={convert}>     
+        <div>
+          <h3>Currency Calculator</h3>
+          <label>Eur </label>&nbsp;
+          <input type="number" step="0.01"
+          value={eur} onChange={e => setEur(e.target.value)} />
+          <div>
+          <output>{rateE}€ = {rate}£</output>
+          </div>
+        </div>
+        <div>
+          <button>Calculate</button>
+        </div>
+        <div>
+          <label>Gbp = </label>
+          <output>{gbp.toFixed(2)}£</output>
+        </div>
+        <div>
+
+        </div>
+      </form>
     </div>
   );
 }
